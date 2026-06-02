8b9c7864567ba1b8181254eba8b9eb6c571460fa
Sushan <85663813+Sushan2589@users.noreply.github.com>
2026-06-02 16:02:23 +0545

    Backend - Login/Signup

---
+++ backend/server.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const db = require("./db");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: 'mysecret',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Example user
+const users = [
+  {
+    username: 'admin',
+    password: bcrypt.hashSync('1234', 10),
+  },
+];
+
+function isAuthenticated(req, res, next) {
+  if (req.session.user) {
+    return next();
+  }
+  res.redirect('/login');
+}
+
+app.get("/signup", (req, res) => {
+  res.send(`
+    <form method="POST" action="/signup">
+      <input name="username" placeholder="Username">
+      <input name="password" type="password" placeholder="Password">
+      <button>Sign Up</button>
+    </form>
+  `);
+});
+
+app.get('/login', (req, res) => {
+    const message =
+    req.query.signup === "success"
+      ? "<p>Account created successfully. Please log in.</p>"
+      : "";
+
+  res.send(`
+    <form method="POST" action="/login">
+      <input name="username" placeholder="Username">
+      <input name="password" type="password" placeholder="Password">
+      <button>Login</button>
+
+      <p>
+      Don't have an account?
+      <a href="/signup">Sign Up</a>
+    </p>
+    </form>
+  `);
+});
+
+app.post("/signup", async (req, res) => {
+  const { username, password } = req.body;
+
+  const hash = await bcrypt.hash(password, 10);
+
+  db.run(
+    "INSERT INTO users(username, password) VALUES (?, ?)",
+    [username, hash],
+    function (err) {
+      if (err) {
+        return res.send("Username already exists");
+      }
+
+      
+      res.redirect("/login?signup=success");
+    }
+  );
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  db.get(
+    "SELECT * FROM users WHERE username = ?",
+    [username],
+    async (err, user) => {
+      if (!user) {
+        return res.send("User not found");
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+
+      if (!match) {
+        return res.send("Wrong password");
+      }
+
+      req.session.user = {
+        id: user.id,
+        username: user.username,
+      };
+
+      res.redirect("/dashboard");
+    }
+  );
+});
+
+app.get('/dashboard', isAuthenticated, (req, res) => {
+  res.send(`
+    <h1>Welcome ${req.session.user.username}</h1>
+    <a href="/logout">Logout</a>
+  `);
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/login');
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
