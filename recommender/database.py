2e4e0107c3c3549cdc1f8d9989d6653cd26bbaee
Sujan <sujanpradhan2078@gmail.com>
2026-06-03 14:57:59 +0545

    Small change

--- recommender/database.py
+++ recommender/database.py
@@ -1,7 +1,7 @@
 import sqlite3
 import os
 
-DB_PATH = os.path.join(os.path.dirname(__file__), "..", "backend", "user.db")
+DB_PATH = os.path.join(os.path.dirname(__file__), "..", "backend", "users.db")
 
 def get_connection():
     conn = sqlite3.connect(DB_PATH)
