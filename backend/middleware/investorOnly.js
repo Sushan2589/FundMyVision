f0d5dbed6b3b636a9e506095cd53e6efb209e095
Sushan <85663813+Sushan2589@users.noreply.github.com>
2026-06-03 21:37:30 +0545

    Removal of main to pull another

--- backend/middleware/investorOnly.js
+++
@@ -1,8 +0,0 @@
-function investorOnly(req, res, next) {
-    if (req.session.user?.role === "investor") {
-        return next();
-    }
-    res.status(403).send("Access denied");
-}
-
-module.exports = investorOnly ;
