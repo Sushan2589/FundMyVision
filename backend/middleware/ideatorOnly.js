de691764ef23afdfe6a6c9646d8be7a0cc0898b4
Sujan <sujanpradhan2078@gmail.com>
2026-06-03 06:45:05 +0545

    back

--- backend/middleware/ideatorOnly.js
+++
@@ -1,8 +0,0 @@
-function ideatorOnly(req, res, next) {
-    if (req.session.user?.role === "ideator") {
-        return next();
-    }
-    res.status(403).send("Access denied");
-}
-
-module.exports = ideatorOnly ;
